5f800ae9184b8397260c933e043d309eecfe18eb
kimsavinfo <kimsavinfo@gmail.com>
2014-05-24 20:59:51 +0200

    getAll ok

--- jsfmvc-with-jdbc/src/main/java/fr/epsi/individu/IndividuRepository.java
+++ jsfmvc-with-jdbc/src/main/java/fr/epsi/individu/IndividuRepository.java
@@ -3,6 +3,7 @@ package fr.epsi.individu;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -49,12 +50,40 @@ public class IndividuRepository {
 		return "individu?faces-redirect=true";
 	}
 
-	public List<Individu> getAll() {
+	public List<Individu> getAll() throws SQLException {
 		List<Individu> result = new ArrayList<>();
+		
+		Connection connection = dataSource.getConnection();
+		try {
+			ResultSet resultSet = null;
+			String request = "SELECT id_individu, nom_individu, prenom_individu, age_individu  FROM INDIVIDUS";
+			
+			PreparedStatement pstmt = connection.prepareStatement(request);
+			pstmt.execute();
+			resultSet = pstmt.getResultSet();
+			
+			while(resultSet.next())
+			{
+				Individu individu = new Individu();
+				individu.setId(resultSet.getLong(1));
+				individu.setNom(resultSet.getString(2));
+				individu.setPrenom(resultSet.getString(3));
+				individu.setAge(resultSet.getInt(4));
+				
+				result.add(individu);
+			}
+		}
+		finally{
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		
 		return result;
 	}
 
 	public void delete(long id) {
+		
 	}
 
 }
